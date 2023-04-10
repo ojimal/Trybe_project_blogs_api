3b5aad6883c3e598822a284b292ad0654acab579
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-04-10 20:55:16 +0200

    update user migration and .env

--- src/migrations/20230408150258-create-user.js
+++ src/migrations/20230408150258-create-user.js
@@ -21,8 +21,7 @@ module.exports = {
         allowNull: false,
       },
       image: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       }
     });
   },
